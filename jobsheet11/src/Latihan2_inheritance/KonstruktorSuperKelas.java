483c75f14291831a4ca806f53b23cbc34f0a1ac4
farhannovaldi <112691854+farhannovaldi@users.noreply.github.com>
2022-11-10 23:09:13 +0700

    Delete jobsheet11 directory

--- jobsheet11/src/Latihan2_inheritance/KonstruktorSuperKelas.java
+++
@@ -1,8 +0,0 @@
-package Latihan2_inheritance;
-
-public class KonstruktorSuperKelas {
-    public static void main(String[] args) {
-        Employ programer1 = new Employ("12345678", "Budi", 33);
-        programer1.info();
-    }
-}
